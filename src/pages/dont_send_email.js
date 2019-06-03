bf67aa0e881501c06c10ce0644ba90e302e61aa4
Chad Jemmett <chad.jemmett@gmail.com>
2019-06-03 14:57:15 -0600

    Fixed the copy for dont send that email

--- src/pages/dont_send_email.js
+++ src/pages/dont_send_email.js
@@ -24,14 +24,13 @@ class Generic extends React.Component {
 
     return (
       <Layout>
-        <Helmet title="Generic Page Title" />
-        <HeaderGeneric />
+        <Helmet title="Don't Send that Email" />
+        <h1>Don't Send that Email</h1>
         <div id="main">
           <section id="content" className="main">
             <span className="image main"><img src={dont_send_email} alt="Chad Jemmett" /></span>
             <h2>Don't Send that Email</h2>
-            <p>This is a React app with a Node backend. This app takes the user's message and analyzes the emotional tone of the message. It uses the IBM Watson API to do the analysis. I worked on a team of six people to get this app completed in five weeks. The most challenging and intersesting thing was implementing the API. We used the IBM Watson API to analyze the tone of email or text messages.ue leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis fames ac ante ipsum primis in faucibus.</p>
-            <p>Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Consequat leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit enim turpis lorem ipsum dolor sit amet feugiat. Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus, lacus eget hendrerit bibendum, urna est aliquam sem, sit amet est velit quis lorem.</p>
+            <p>This is a React app with a Node backend. This app takes the user's message and analyzes the emotional tone of the message. It uses the IBM Watson API to do the analysis. I worked on a team of six people to get this app completed in five weeks. The most challenging and interesting thing was implementing the API. We used the IBM Watson API to analyze the tone of email or text messages.</p>
             <div style={linkDiv}>
             <a style={linkStyle} href="https://github.com/Lambda-School-Labs/dont-send-that-email">Github</a>
             <a style={linkStyle} href="https://dont-send-that-email.netlify.com/">Live Site</a>
