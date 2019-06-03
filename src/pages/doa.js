5d5d99948cf941e5a7760fb7a17bb6163589ee81
Chad Jemmett <chad.jemmett@gmail.com>
2019-06-03 16:58:52 -0600

    Wrote the copy for doa

--- src/pages/doa.js
+++ src/pages/doa.js
@@ -1,25 +1,58 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-
+import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import HeaderGeneric from '../components/HeaderGeneric'
+import dont_send_email from '../assets/images/dont_send_email.png'
 import pic04 from '../assets/images/pic04.jpg'
 
+const linkDiv = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}
+
+const linkStyle = {
+  // color: "red",
+  border: "solid black 1px",
+  width: "20%",
+  margin: "0 1%",
+  textAlign: "center"
+}
+
+
+const imgStyle = {
+  height: "150px"
+}
+
+const headerStyles = {
+  margin: "30px 0",
+
+}
+
+const headerDiv= {
+  display: "flex",
+}
 class Generic extends React.Component {
   render() {
 
     return (
       <Layout>
         <Helmet title="Dead or Alive" />
+        <div style={headerDiv}>
+        <h1 style={headerStyles}>Dead or Alive</h1>
+        </div>
         <div id="main">
           <section id="content" className="main">
-            <span className="mage main"><img src={pic04} alt="" /></span>
+            <span className="image main" ><img src={pic04} alt="Dead or alive" /></span>
             <h2>Dead or Alive</h2>
-            <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis fames ac ante ipsum primis in faucibus.</p>
-            <p>Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Consequat leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit enim turpis lorem ipsum dolor sit amet feugiat. Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus, lacus eget hendrerit bibendum, urna est aliquam sem, sit amet est velit quis lorem.</p>
-            <h2>Tempus veroeros</h2>
-            <p>Cep risus aliquam gravida cep ut lacus amet. Adipiscing faucibus nunc placerat. Tempus adipiscing turpis non blandit accumsan eget lacinia nunc integer interdum amet aliquam ut orci non col ut ut praesent. Semper amet interdum mi. Phasellus enim laoreet ac ac commodo faucibus faucibus. Curae ante vestibulum ante. Blandit. Ante accumsan nisi eu placerat gravida placerat adipiscing in risus fusce vitae ac mi accumsan nunc in accumsan tempor blandit aliquet aliquet lobortis. Ultricies blandit lobortis praesent turpis. Adipiscing accumsan adipiscing adipiscing ac lacinia cep. Orci blandit a iaculis adipiscing ac. Vivamus ornare laoreet odio vis praesent nunc lorem mi. Erat. Tempus sem faucibus ac id. Vis in blandit. Nascetur ultricies blandit ac. Arcu aliquam. Accumsan mi eget adipiscing nulla. Non vestibulum ac interdum condimentum semper commodo massa arcu.</p>
+             <p>I wrote the backend of this app in Node. The front-end developer and I collaborated on the database schema. I wrote tests to ensure the enpoints worked properly. This was an enjoyable project. I was the sole developer on the backend. I had to research and implement the usage of third-party APIs. I used two different APIs. One to get the data about the famous people. The other to get the pictures. <br /><strong>Note: There are no pull requests on this app. My project manager and I just pushed or merged to master after we discussed the feature</strong> </p>
+            <div style={linkDiv}> <a style={linkStyle} href="https://github.com/BrianARuff/team_dead_or_alive/tree/master/api">Github</a>
+            <a style={linkStyle} href="https://doa-dev-marketing.herokuapp.com">Live Site</a>
+            <a style={linkStyle} href="#">Pull Requests</a>
+            </div>
           </section>
+
         </div>
       </Layout>
     )
