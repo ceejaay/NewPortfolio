6de79775d9e61b5c7f631d815636ef5c4719d2fd
Chad Jemmett <chad.jemmett@gmail.com>
2019-06-03 14:32:33 -0600

    Wrote in some copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -74,10 +74,10 @@ class Index extends React.Component {
                 <header className="major">
                   <h2>A.B.C. Always Be Creating.</h2>
                 </header>
-                <p>So it's not gramattically correct. But  we have a short time on this rock. It's up to us to make it worthwhile.
-                Fast learner, focused, independant. Leader. These are the things you get with me.
-                Please, take a look at my projects. Contact me with any questions.
+                <p>Life is short, so don't bother wasting time on things you are not passionate about.
+                I am passionate about building tools that help people communicate. I am an independant, reliable full-stack web dev.
                 </p>
+                <p>Please, take a look at my projects. Contact me with any questions.</p>
                 <ul className="actions">
                 </ul>
               </div>
@@ -114,8 +114,8 @@ class Index extends React.Component {
           <section id="second" className="main special">
             <header className="major">
               <h2>Skills</h2>
-              <p>I like learning. Here's a sample of my skills.se feugiat congue<br />
-              posuere. Nulla massa urna, fermentum eget quam aliquet.</p>
+              <p>I always like learning new things. But this is what I'm best at.<br />
+              </p>
             </header>
             <ul className="statistics">
               <li className="style1">
@@ -139,7 +139,7 @@ class Index extends React.Component {
                 <strong></strong> Python
               </li>
             </ul>
-            <p className="content">Javascript, Node, React. These are the front-end and backend skills that are required. I taught myself Ruby so I know how to read documentation. I gained computer science prinicpals using Python.</p>
+            <p className="content">To learn programming, I taught myself Ruby. To round out my skills I went to <a href="https://lambdaschool.com/courses/cs/web/">Lambda School</a>. There I learned Javascript, React, Node and Python. Lambda School is a 30 week hands-on curriculum focused on computer science, software engineering and web development. I have a solid foundation of web development and computer science.</p>
             <footer className="major">
               <ul className="actions">
               </ul>
