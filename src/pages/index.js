62ce05235360a3c28876f64ffca0353302764336
Chad Jemmett <chad.jemmett@gmail.com>
2019-06-07 13:40:36 -0600

    one page seems to be working nicely

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import { Link } from 'gatsby'
 import Helmet from 'react-helmet'
 import Waypoint from 'react-waypoint'
 
+import Scrollspy from 'react-scrollspy'
+import Scroll from '../components/Scroll'
 import Layout from '../components/layout'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
@@ -25,6 +27,12 @@ const tempIcon = {
   width: "100px",
   height: "100px",
   fill: "#ffffff",
+}
+
+const projLiStyle = {
+  display: "flex",
+  flexDirection: "column",
+
 }
 
 
@@ -91,18 +99,23 @@ class Index extends React.Component {
             </header>
             <ul className="features">
               <li>
-                <Link to="doa">
-                  <img style={imgStyle} src={dead} alt="pic"/>
-                  <h3>Dead or Alive?</h3>
-                  <p>A React app that tests a user on whether a famous person is living or dead? I wrote the backend code.</p>
-                </Link>
+                <Scroll type="id" element="doa">
+                  <a href="#">
+                    <img style={imgStyle} src={dead} alt="pic"/>
+                    <h3>Dead or Alive?</h3>
+                    <p>A React app that tests a user on whether a famous person is living or dead? I wrote the backend code.</p>
+                  </a>
+                </Scroll>
               </li>
               <li>
-                <Link to="dont_send_email">
-                  <img style={imgStyle} src={dont} alt="pic"/>
-                  <h3>Don't Send that Email</h3>
-                  <p>A React app that evaluates the emotional tone of an email or text message.</p>
-                </Link>
+                <Scroll type="id" element="email">
+                  <a href="#">
+                    <img style={imgStyle} src={dont} alt="pic"/>
+                    <h3>Don't Send that Email</h3>
+                    <p>A React app that evaluates the emotional tone of an email or text message.</p>
+                  </a>
+
+                </Scroll>
               </li>
             </ul>
             <footer className="major">
@@ -110,7 +123,47 @@ class Index extends React.Component {
               </ul>
             </footer>
           </section>
+          <section id="email" className="main special">
+            <header className="major">
+              <h2>Don't Send that Email</h2>
+              <p><br />
+              </p>
+            </header>
+            <ul className="features">
+              <li>
+                <img style={imgStyle} src={dont} alt="pic"/>
+              </li>
+              <li style={projLiStyle}>
+            <a  href="https://github.com/Lambda-School-Labs/dont-send-that-email">Github</a>
+            <a  href="https://dont-send-that-email.netlify.com/">Live Site</a>
+            <a  href="https://github.com/Lambda-School-Labs/dont-send-that-email/pulls?q=is%3Apr+author%3Aceejaay+is%3Aclosed">Pull Requests</a>
+              </li>
+            </ul>
+            <p className="content">
+              This is a React app with a Node backend. This app takes the user's message and analyzes the emotional tone of the message. It uses the IBM Watson API to do the analysis. I worked on a team of six people to get this app completed in five weeks. The most challenging and interesting thing was implementing the API. We used the IBM Watson API to analyze the tone of email or text messages. For this app, my team and I did regular code reviews and followed a professional git workflow.
+            </p>
+            <footer className="major">
+              <ul className="actions">
+              </ul>
+            </footer>
+          </section>
 
+          <section id="doa" className="main special">
+            <header className="major">
+              <h2>Dead or Alive</h2>
+              <p><br />
+              </p>
+            </header>
+            <ul className="features">
+              <li>image</li>
+              <li>links</li>
+            </ul>
+            <p className="content">description of project here</p>
+            <footer className="major">
+              <ul className="actions">
+              </ul>
+            </footer>
+          </section>
           <section id="second" className="main special">
             <header className="major">
               <h2>Skills</h2>
@@ -144,6 +197,9 @@ class Index extends React.Component {
               <ul className="actions">
               </ul>
             </footer>
+          </section>
+          <section id="test">
+            this is the test section
           </section>
 
 
